20e0193c0eb8452020fb078f44224395fccaa4cb
Josh <nzryand@gmail.com>
2022-02-02 22:36:56 +0100

    Whitelisting more

--- src/checkDomain.js
+++ src/checkDomain.js
@@ -2,7 +2,7 @@ export const checkDomain = (req, res, next) => {
     const domain = req.get('host') || req.get(`host`)
     const whiteList = [
       `localhost:5000`,
-      `wbroadconditionapi.herokuapp.com`
+      `wbroadcondition.web.app`,
     ]
     if(whiteList.includes(domain)) next()
     else res.status(404).send()
